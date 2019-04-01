c55137196e0a50d2c684106d58e626e82f0efb17
Michael Henderson <Michael.Henderson@plateauinc.com>
2019-04-01 15:31:39 -0500

    being laying down code that will connect the user to azure dev ops.

--- src/TensileStrength.Console/Program.cs
+++ src/TensileStrength.Console/Program.cs
@@ -7,7 +7,67 @@ namespace TensileStrength.Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine(args);
+
+            if (args.Length > 1)
+            {
+                BeginExecution(args);
+            } else
+            {
+                System.Console.WriteLine("You must provide two parameters. EX: TensileStrength.exe ado login. The first parameter gives the console app context and the second is the action");
+            }
+           
+        }
+
+        private static void BeginExecution(string[] args)
+        {
+            var context = args[0];
+            var action = args[1];
+
+            if(context == "ado" && action == "login")
+            {
+                AdoLogin();
+            }
+          
+        }
+
+        private static void AdoLogin()
+        {
+
+            string clientAppId;
+            string clientAppSecret;
+            string scope;
+            string callBackUrl;
+
+            while (true)
+            {
+                WriteConsoleLine("Azure Dev Ops Client App Id: ");
+                clientAppId = ReadConsoleLine();
+                WriteConsoleLine("Azure Dev Ops Client App Secret: ");
+                clientAppSecret = ReadConsoleLine();
+                WriteConsoleLine("Azure Dev Ops Scope: ");
+                scope = ReadConsoleLine();
+                WriteConsoleLine("Azure Dev Ops Callback URL: ");
+                callBackUrl = ReadConsoleLine();
+                WriteConsoleLine("Connection to Azure Dev Ops Successful...", status: "success");
+                break;
+            }
+        }
+
+        private static string ReadConsoleLine()
+        {
+            string line = System.Console.ReadLine();
+            return line;
+        }
+
+        private static void WriteConsoleLine(string output, string status = null)
+        {
+            if(status == "success")
+            {
+                System.Console.ForegroundColor = ConsoleColor.Green;
+            }
+            System.Console.WriteLine(output);
+            System.Console.ResetColor();
         }
     }
 }
